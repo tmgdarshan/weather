9406a2d0da105e8668dd9fe4bebbfa1eac718a3a
tmgdarshan <darshantamang@gmail.com>
2025-11-10 10:56:10 +0100

    refactoring in the weather_process.py

--- src/examples/refactoring/weather_process.py
+++ src/examples/refactoring/weather_process.py
@@ -1,35 +1,55 @@
 """This script is badly written on purpose to demonstrate refactoring."""
+
 import csv, math
 
-def f(a):
-    t=[]
-    for i in a:
-        if float(i[1])>25:
-            t.append(float(i[1])*1.8+32)
-        else:
-            t.append(float(i[1]))
-    return t
+# Constants
+Temp_Threshold = 25
+Fahrenheit_Offset = 32
+Celsius_to_Fahrenheit_Factor = 1.8
 
-def g(a):
-    s=0
+# Load weather data
+Path = "./../../../data/weather_data.csv"
+
+
+# Convert temp in Celsius to Fahrenheit above the threshold temp
+def convert_temp(a):
+    temp_C = []
     for i in a:
-        s+=i
+        if float(i[1]) > Temp_Threshold:
+            temp_C.append(
+                float(i[1]) * Celsius_to_Fahrenheit_Factor + Fahrenheit_Offset
+            )
+        else:
+            temp_C.append(float(i[1]))
+    return temp_C
+
+
+def calculate_sum(a):
+    s = 0
+    for i in a:
+        s += i
     return s
 
 
-r=open('./../../../data/weather_data.csv')
-d=list(csv.reader(r))
-r.close()
-d=d[1:]
-x=[]
-for i in d:
-    x.append([i[0],i[1],i[2],i[3],i[4]])
-y=f(x)
-z=g(y)
-print('sum',z)
-print('avg',z/(len(y) if len(y) else 1))
-ws=0
-for i in d:
-    u=float(i[3]); v=float(i[4])
-    ws+=math.sqrt(u*u+v*v)
-print('wind',ws/len(d))
+# Load the weather data
+data_path = open(Path)
+data_load = list(csv.reader(data_path))
+data_path.close()
+weather_data = data_load[1:]
+x = []
+
+# Temperature statistics
+for i in weather_data:
+    x.append([i[0], i[1], i[2], i[3], i[4]])
+total_temps = convert_temp(x)
+sum_of_temps = calculate_sum(total_temps)
+print("sum", sum_of_temps)
+print("avg", sum_of_temps / (len(total_temps) if len(total_temps) else 1))
+
+# Wind Statistics
+total_wind = 0
+for i in weather_data:
+    u = float(i[3])
+    v = float(i[4])
+    total_wind += math.sqrt(u * u + v * v)
+print("wind", total_wind / len(data_load))
